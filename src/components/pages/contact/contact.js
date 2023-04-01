e06406586004a150e0def1beea27c5442b64ca07
PennyLIU2022 <114879207+PennyLIU2022@users.noreply.github.com>
2023-04-01 12:43:01 -0400

    add email validating check

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Container, Col, Form, Row } from "react-bootstrap";
+import { validateEmail } from "../../../utils/validateEmail";
 import "./contact.css";
 
 function Contact() {
@@ -27,6 +28,12 @@ function Contact() {
       alert("Name, Email or Message is missing, please fill all fields");
       return;
     }
+
+    if (!validateEmail(email)) {
+      alert("Not a valid email address, please try again!");
+      return;
+    }
+
     alert(`Thank you for your message, ${Name}!`);
     setName("");
     setEmail("");
@@ -35,76 +42,76 @@ function Contact() {
   return (
     <Container fluid className="contact" id="contact">
       <Container className="contact-container">
-          <Row size={12} md={12}>
-            <h1> Contact me </h1>
-            <p> Feel free to reach me at any time</p>
-            <Form className="contact-form">
-              <Form.Group className="mb-4">
-                <Form.Label>
-                  <h5>
-                    <strong>Name</strong>
-                  </h5>
-                </Form.Label>
-                <Form.Control className="form-message"
-                  type="text"
-                  placeholder="Name"
-                  name="Name"
-                  value={Name}
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
+        <Row size={12} md={12}>
+          <h1> Contact me </h1>
+          <p> Feel free to reach me at any time</p>
+          <Form className="contact-form">
+            <Form.Group className="mb-4">
+              <Form.Label>
+                <h5>
+                  <strong>Name</strong>
+                </h5>
+              </Form.Label>
+              <Form.Control
+                className="form-message"
+                type="text"
+                placeholder="Name"
+                name="Name"
+                value={Name}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
 
-              <Form.Group className="mb-4">
-                <Form.Label>
-                  <h5>
-                    <strong>Email Address</strong>
-                  </h5>
-                </Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="name@example.com"
-                  name="email"
-                  value={email}
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label>
+                <h5>
+                  <strong>Email Address</strong>
+                </h5>
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
 
-              <Form.Group
-                className="mb-4"
-                controlId="exampleForm.ControlTextarea1"
+            <Form.Group
+              className="mb-4"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>
+                <h5>
+                  <strong>Message</strong>
+                </h5>
+              </Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                name="message"
+                value={message}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <div
+              className="submit"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <button
+                type="submit"
+                className="submit-btn"
+                onClick={handleFormSubmit}
               >
-                <Form.Label>
-                  <h5>
-                    <strong>Message</strong>
-                  </h5>
-                </Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows={3}
-                  name="message"
-                  value={message}
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
-
-              <div
-                className="submit"
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <button
-                  type="submit"
-                  className="submit-btn"
-                  onClick={handleFormSubmit}
-                >
-                  <span>Submit</span>
-                </button>
-              </div>
-            </Form>
-          </Row>
-        
+                <span>Submit</span>
+              </button>
+            </div>
+          </Form>
+        </Row>
       </Container>
     </Container>
   );
